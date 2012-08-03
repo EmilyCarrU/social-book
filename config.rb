552c0b0c8bdf7009b7d153d20068227a11481850
Kenneth Ormandy <hello@kennethormandy.com>
2012-08-02 17:03:43 -0700

    Compass 0.13
    Compass 0.13 alpha
    Stitch, Compass plugin
    Broke Hyphenate again
    Properly included Pinchcard

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require 'stitch'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -12,13 +13,8 @@ environment = :development
 
 relative_assets = true
 
+
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
 color_output = false
 
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
