dec0d98b511f9971390481362c17196af08e6270
Kenneth Ormandy <hello@kennethormandy.com>
2012-08-13 16:54:48 -0700

    Updated markup

--- section_templates/comment_columns.php
+++ section_templates/comment_columns.php
@@ -1,50 +1,56 @@
 </section><!-- / .chap_sec -->
 <div class="target target__com">
-	    <section class="com">
+	<aside class="com">
     	<span class="ui com_count"><?php echo(rand(1,15)); ?></span>
-    	<span class="com_dive"></span><!-- maybe change this to an i? -->
-    	<div class="com_wrapper">
+    	<i class="com_dive"></i>
+    	<section class="com_wrapper">
     		<ul class="com_comments">
     			<li class="com_comments_comment">
-    				<div class="com_comments_comment_wrap">
+    				<section class="com_comments_comment_wrap">
     					<p>I think this is a good book.</p>
-    					<cite class="com_comments_comment_cite">&mdash;Bob Jeremy</cite><!-- Check about this tag -->
-
-    				</div><!-- / com_comments_comment_wrap -->
-    				<a href="http://google.com" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
+    					<span class="com_comments_comment_cite">&mdash;Bob Jeremy</span>
+    				</section><!-- / com_comments_comment_wrap -->
+    				<a href="#reply" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
     					<i class="ui com_comments_comment_reply_icon">+</i>Reply
     				</a><!-- / .ui com_comments_comment_reply -->
     			</li><!-- / r.com_comments_comment -->
     			<li class="com_comments_comment">
-    				<div class="com_comments_comment_wrap">
+    				<section class="com_comments_comment_wrap">
     					<p>For example, &#8216;mobile&#8217; becomes an icon of a mobile device. Pretty neat, although I wonder how practical it is&hellip;</p>
-    					<cite class="com_comments_comment_cite">&mdash;Brad Frost</cite>
-    				</div><!-- / com_comments_comment_wrap -->
-    				<a href="http://google.com" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
+    					<span class="com_comments_comment_cite">&mdash;Brad Frost</span>
+    				</section><!-- / com_comments_comment_wrap -->
+    				<a href="#reply" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
     					<i class="ui com_comments_comment_reply_icon">+</i>Reply
     				</a><!-- / .ui com_comments_comment_reply -->
-    			</li><!-- / .com_comments_comment -->
+    			</li><!-- / .com_comments_comment -->    			
     			<li class="com_comments_comment">
-    				<div class="com_comments_comment_wrap">
+    				<section class="com_comments_comment_wrap">
     				<p>Symbolset is a tool that uses a custom font to serve up a unicode equivalent of certain key words. This is a thing. That is a thing&hellip;</p>
-    				<cite class="com_comments_comment_cite">&mdash;Person Withareallylongnamethatislong</cite>
-    				</div><!-- / com_comments_comment_wrap -->
-    				<a href="http://google.com" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
+    				<span class="com_comments_comment_cite">&mdash;Person Withareallylongnamethatislong</span>
+    				</section><!-- / com_comments_comment_wrap -->
+    				<a href="#reply" title="Reply to Brad Frost's comment" class="ui com_comments_comment_reply">
     					<i class="ui com_comments_comment_reply_icon">+</i>Reply
     				</a><!-- / .ui com_comments_comment_reply -->
     				<!-- <div class="clear_temp"></div><!-- Hopefully I can get rid of this -->
     			</li><!-- / .com_comments_comment -->
-    			<section class="com_comments_actions">
-    				<a href="#comment" title="Add a new comment to this discussion" class="button com_comments_meta_add">Add Comment</a>
-    			</section><!-- / .com_comments_meta -->
+    			<li class="com_comments_actions">
+	    			<ul class="com_comments_actions_items">
+		    			<li class="com_comments_actions_item">
+    						<a href="#comments" title="View all the comments in this discussion">View all comments&hellip;</a>
+	    				</li>
+	    				<li class="com_comments_actions_item">
+		    				<a href="#comment" title="Add a new comment to this discussion" class="button com_comments_meta_add">Add Comment</a>
+		    			</li><!-- / .com_comments_meta -->
+	    			</ul><!-- / .com_comments_actions -->
+    			</li>
     			<!-- Not using the fourth comment for now
     			<li class="com_comments_comment">
     				<p>A thorough explanation on how to set up a device lab for mobile web testing. And this is just part one.</p>
-    				<cite class="com_comments_comment_cite">&mdash;Dave Olsen</cite>
+    				<span class="com_comments_comment_cite">&mdash;Dave Olsen</span>
     			</li><!-- / .com_comments_comment -->
     			<!-- <div class="clear_temp"></div><!-- Hopefully I can get rid of this -->
     		</ul><!-- / .com_comments -->
-    	</div><!-- / .com_wrapper, hopefully remove -->
-	</section>
+    	</section><!-- / .com_wrapper, hopefully remove -->
+	</aside>
 </div><!-- / .target__com -->
 <section class="chap_sec">
