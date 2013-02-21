6017841c7b61131d57dfe0c5cb010a5ba71dd6f2
kennethormandy <hello@kennethormandy.com>
2013-02-21 12:33:04 -0800

    Toggle click/tap/pinch in settings
    Doesn't work for comments yet
    Added touch event testing to Modernizr build

--- www/js/application.js
+++ www/js/application.js
@@ -4,9 +4,19 @@ var apiEndpoint = 'http://book.hyko.org/api';
 
 window.App = {};
 window.App.online = false;
+window.App.optionClick = true; // no effect on comments yet
+window.App.optionPinch = true; // no effect on comments yet
+window.App.optionTap = false; // no effect on comments yet
+
+
+if(!Modernizr.touch && window.App.optionClick && !window.App.optionTap) {
+  window.App.optionClickDev = true; // for when we need to test the pinch on touch, and still want to click on desktop
+}
+
+
 //
 // Models
-// 
+//
 
 App.Decade = Backbone.Model.extend({
   title: '',
@@ -25,7 +35,7 @@ App.Year = Backbone.Model.extend({
       this.set('decade', parseInt(m[1]));
       this.set('year', parseInt(m[2]));
       this.set('part', parseInt(m[3]));
-    } 
+    }
   }
 });
 
@@ -36,7 +46,7 @@ App.Chapter = Backbone.Model.extend({
   content : '',
   comment_status : false,
   decade: null,
-  
+
   initialize: function(o) {
 
     var m = o.slug.match(/(\d{4})-(\d{2})-(\d{2})/);
@@ -45,7 +55,7 @@ App.Chapter = Backbone.Model.extend({
       this.set('year', parseInt(m[2]));
       this.set('part', parseInt(m[3]));
     }
-    
+
     this.comments = new App.Comments(o.comments);
   }
 });
@@ -60,7 +70,7 @@ App.Comment = Backbone.Model.extend({
        complete: function(xhr, status) {
          // Done... console.log(xhr, status)
        }
-     }) 
+     })
     }
   }
 });
@@ -73,7 +83,7 @@ App.Years = Backbone.Collection.extend({
   model: App.Year,
   initialize: function(o) {
   },
-  
+
   comparator: function(year1, year2) {
     if (year1.get('year') < year2.get('year')) {
       return 1;
@@ -89,7 +99,7 @@ App.Years = Backbone.Collection.extend({
       }
     }
   },
-  
+
   parse: function(response) {
     return response.posts;
   }
@@ -99,18 +109,18 @@ App.Years = Backbone.Collection.extend({
 App.Decades = Backbone.Collection.extend({
   model: App.Decade,
   url: apiEndpoint + "/get_tag_posts/?tag=decade",
-  
+
   initialize: function(d) {
   },
 
   comparator: function(decade) {
     return decade.get('decade');
   },
-  
+
   parse: function(response) {
     return response.posts;
   }
-  
+
 });
 
 App.Chapters = Backbone.Collection.extend({
@@ -122,7 +132,7 @@ App.Chapters = Backbone.Collection.extend({
   //     return 1;
   //   } else if (chapter1.get('decade') > chapter2.get('decade')) {
   //     return -1;
-  //   } else if (chapter1.get('decade') === chapter2.get('decade')) {   
+  //   } else if (chapter1.get('decade') === chapter2.get('decade')) {
   //     if (chapter1.get('year') < chapter2.get('year')) {
   //       return 1;
   //     } else if (chapter1.get('year') > chapter2.get('year')) {
@@ -138,11 +148,11 @@ App.Chapters = Backbone.Collection.extend({
   //     }
   //   }
   // },
-  
+
   parse: function(response) {
     return response.posts;
   }
-  
+
 });
 
 App.Comments = Backbone.Collection.extend({
@@ -154,29 +164,29 @@ App.Comments = Backbone.Collection.extend({
 // Views
 //
 
-  
+
 App.CommentView = Backbone.View.extend({
   tagName: "li",
   className: "com_comments_comment",
-  
+
   events: {
     "click": "preventDefault",
     "tap .com_comments_comment_reply"     : "addComment",
     "click .com_comments_comment_reply"   : "addComment"
   },
-  
+
   preventDefault: function(e) {
     e.preventDefault();
     e.stopPropagation();
   },
-  
+
   // This is the reply button
   addComment: function(e) {
     e.preventDefault();
     e.stopPropagation();
     alert("Add Reply");
   },
-    
+
   render: function() {
     var template =  _.template($("#template-comment").html());
     var html = template(this.model.toJSON());
@@ -184,11 +194,11 @@ App.CommentView = Backbone.View.extend({
     $(this.el).append(html);
     return this;
   }
-  
+
 });
 
 App.CommentsView = Backbone.View.extend({
-  
+
   events: {
 
     "click": "preventDefault",
@@ -201,21 +211,21 @@ App.CommentsView = Backbone.View.extend({
     "click .cancel" : "cancelCommentForm",
     "click .com_add_form .submit": "addComment"
   },
-  
+
   initialize: function() {
     this.model.bind('change', this.updateCount, this);
   },
-  
+
   preventDefault: function(e) {
     e.preventDefault();
     e.stopPropagation();
   },
-  
+
   // This is the add button on the comments for the form
   showCommentForm : function(e) {
     e.preventDefault();
     e.stopPropagation();
-    
+
     // Add the Comment Form
     var template =  _.template($("#template-comment-form").html());
     var html = template();
@@ -223,22 +233,22 @@ App.CommentsView = Backbone.View.extend({
     // TODO, move this upto init or render.
     $(this.el).append(html);
   },
-  
+
   clearForm: function() {
     $(this.el).find('.com_add_form #commentName').val('');
     $(this.el).find('.com_add_form #commentEmail').val('');
-    $(this.el).find('.com_add_form #commentText').val('');    
+    $(this.el).find('.com_add_form #commentText').val('');
   },
-  
+
   closeForm: function() {
     $(this.el).find('.modal').toggleClass("modal__open");
     var that = this;
     window.setTimeout(function(){
-      $(that.el).find('.com_add_sec').remove();      
+      $(that.el).find('.com_add_sec').remove();
     },1000);
 
   },
-  
+
   addComment: function(e) {
     e.preventDefault();
     e.stopPropagation();
@@ -249,33 +259,33 @@ App.CommentsView = Backbone.View.extend({
     attr.email = $(this.el).find('.com_add_form #commentEmail').val();
     attr.content = $(this.el).find('.com_add_form #commentText').val();
     attr.post_id = this.model.get('id');
-  
+
     this.collection.create(attr);
     this.clearForm();
     this.closeForm();
 
     // Update the comment count
-    this.model.set('comment_count', this.model.get('comment_count') + 1);   
+    this.model.set('comment_count', this.model.get('comment_count') + 1);
   },
-  
+
   cancelCommentForm: function(e) {
     e.preventDefault();
     e.stopPropagation();
 
     this.closeForm();
   },
-  
+
   toggleComments : function(e) {
     e.preventDefault();
     e.stopPropagation();
     $(this.el).find('.dive').first().toggleClass('dive__open');
   },
-  
+
   // Called on 'change'
   updateCount: function() {
     $(this.el).find('.dive_count').html(this.model.get('comment_count'));
   },
-  
+
   render : function() {
 
     var template =  _.template($("#template-comments").html());
@@ -285,7 +295,7 @@ App.CommentsView = Backbone.View.extend({
     if (this.model.get('comment_count') === 0) {
       $(this.el).find('.com_comments_actions_item .view_all').hide();
     }
-    
+
     this.collection.each(function(comment) {
       var commentView = new App.CommentView({ model : comment });
       $(this.el).find('.com_comments').prepend(commentView.render().el);
@@ -300,13 +310,13 @@ App.SectionView = Backbone.View.extend({
   initialize: function() {
     this.model.comments.bind('add', this.addOne, this);
   },
-  
+
   render : function() {
     // Main section template
     var template =  _.template($("#template-section").html());
     var html = template(this.model.toJSON());
     $(this.el).append(html);
-    
+
     // Comments template
     if (this.model.get('comment_status') === 'open') {
       var commentsView = new App.CommentsView({
@@ -335,7 +345,7 @@ App.SectionView = Backbone.View.extend({
     // Hit the server with the comment.
     // console.log(comment.toJSON());
   }
-  
+
 });
 
 App.DecadeView = Backbone.View.extend({
@@ -343,26 +353,26 @@ App.DecadeView = Backbone.View.extend({
   className: 'toc_item',
 
   events: {
-    // "click .target__decade_toc" : "expandItem",
-    // "tap .target__decade_toc" : "expandItem",
+    "click .target__decade_toc" : "expandItemClick",
+    "tap .target__decade_toc" : "expandItemTap",
     "openPanel .target__decade_toc" : "expandItemPinch",
     "clsePanel .target__decade_toc" : "clapseItemPinch"
   },
-    
+
   initialize: function() {
     // Init the years view
-    var that = this;    
+    var that = this;
     this.years = new App.Years(chapters);
-    
-    
+
+
     var decadeItor = function(o) {
       if (o.attributes.year) {
         return (o.attributes.decade == that.model.get('decade')  && o.attributes.part == 1) ? true : false;
       }
     }
-    
+
     // Filter out the the other decades
-    var yearList = that.years.filter(decadeItor);    
+    var yearList = that.years.filter(decadeItor);
     this.years.reset(yearList);
 
 
@@ -375,40 +385,57 @@ App.DecadeView = Backbone.View.extend({
     // },this);
 
   },
-  
+
   expandItemPinch : function(e){
     e.preventDefault();
     e.stopPropagation();
 
-    var diveWrapper = $(this.el).find('.dive').first();
-    diveWrapper.addClass('dive__open');
+    if (window.App.optionPinch) {
+      var diveWrapper = $(this.el).find('.dive').first();
+      diveWrapper.addClass('dive__open');
+    }
   },
 
-  expandItem : function(e){
+  expandItemTap : function(e){
     e.preventDefault();
     e.stopPropagation();
-    updateSpine(this.model.get('decade'));
 
-    var diveWrapper = $(this.el).find('.dive').first();
-    // Would using children or something be faster?
-    // Ask Rob about how efficient this is.
-
-    // Kenneth's dive tease test
-    // diveWrapper.addClass('dive__open dive__tease');
-    // window.setTimeout(function(){$(diveWrapper).removeClass("dive__open dive__tease");}, 100);
-    diveWrapper.toggleClass('dive__open');
+    if (window.App.optionTap) {
+      updateSpine(this.model.get('decade'));
+      var diveWrapper = $(this.el).find('.dive').first();
+      diveWrapper.toggleClass('dive__open');
+    }
   },
-  
+
+  expandItemClick : function(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (window.App.optionClickDev) {
+      updateSpine(this.model.get('decade'));
+      var diveWrapper = $(this.el).find('.dive').first();
+      // Would using children or something be faster?
+      // Ask Rob about how efficient this is.
+
+      // Kenneth's dive tease test
+      // diveWrapper.addClass('dive__open dive__tease');
+      // window.setTimeout(function(){$(diveWrapper).removeClass("dive__open dive__tease");}, 100);
+      diveWrapper.toggleClass('dive__open');
+    }
+  },
+
   clapseItemPinch : function(e){
     e.preventDefault();
     e.stopPropagation();
 
-    var diveWrapper = $(this.el).find('.dive').first();
-    diveWrapper.removeClass('dive__close');
+    if (window.App.optionPinch) {
+      var diveWrapper = $(this.el).find('.dive').first();
+      diveWrapper.removeClass('dive__close');
+    }
   },
 
   render: function() {
-    
+
     // Renders the DecadeView Template
     var template =  _.template($("#template-decade").html());
     var html = template(this.model.toJSON());
@@ -421,19 +448,19 @@ App.DecadeView = Backbone.View.extend({
     $(this.el).find('.decade_toc_wrapper').html(this.yearsView.render().el);
     return this;
   }
-  
+
 });
 
 App.DecadesView = Backbone.View.extend({
   tagName : 'ol',
   className : 'decade',
-  
+
   render : function() {
     this.collection.each(function(d) {
       var decadeView = new App.DecadeView({ model : d });
       $(this.el).append(decadeView.render().el);
     }, this);
-    
+
     return this;
   }
 });
@@ -466,7 +493,7 @@ App.YearsView = Backbone.View.extend({
       var yearView = new App.YearView({ model : year });
       $(this.el).prepend(yearView.render().el);
     }, this);
-    
+
     return this;
   }
 });
@@ -476,34 +503,34 @@ App.DecadeIntroView = Backbone.View.extend({
     this.el = $("#decadeIntro");
     this.template = _.template($('#template-decades-header').html());
   },
-  
+
   render: function() {
     this.el.html(this.template(this.model.toJSON()));
     return this;
   }
-  
+
 });
-  
+
 App.YearView = Backbone.View.extend({
   tagName: 'li',
   className : 'yearItem',
   yearToggle: 0,
   events: {
-    // "click": "preventDefault",
-    // "tap .target__chapterItem": "launch",
-    // "click .target__chapterItem": "launch",
+    "click": "preventDefault",
+    "tap .target__chapterItem": "launchTap",
+    "click .target__chapterItem": "launchClick",
     "openPanel .target__chapterItem": "expandItemPinch",
     "clsePanel .target__chapterItem": "clapseItemPinch"
   },
-  
+
   initialize: function(o) {
     var that = this;
     this.chapters = new App.Chapters(chapters);
-    
+
     var itor = function(n) {
       return (n.attributes.decade == that.model.get('decade')) ? true : false;
     }
-    
+
     var chapterList = that.chapters.filter(itor);
     this.chapters.reset(chapterList);
   },
@@ -512,45 +539,67 @@ App.YearView = Backbone.View.extend({
     e.preventDefault();
     e.stopPropagation();
 
-    if (this.yearToggle === 1) {
-      $(this.el).find('.dive').first().addClass('dive__open');
-    } else {
-      $(this.el).find('.dive').first().addClass('dive__open');
-      showYear(this.model.id);
-      updateSpine(this.model.get('decade') + '-' + this.model.get('year'));
+    if(window.App.optionPinch) {
+      if (this.yearToggle === 1) {
+        $(this.el).find('.dive').first().addClass('dive__open');
+      } else {
+        $(this.el).find('.dive').first().addClass('dive__open');
+        showYear(this.model.id);
+        updateSpine(this.model.get('decade') + '-' + this.model.get('year'));
+      }
+      this.yearToggle ^= 1;
     }
-    this.yearToggle ^= 1;
   },
 
-  launch: function(e) {
+  launchClick: function(e) {
     e.preventDefault();
-    if (this.yearToggle === 1) {
-      $(this.el).find('.dive').first().toggleClass('dive__open');
-    } else {
-      $(this.el).find('.dive').first().toggleClass('dive__open');
-      showYear(this.model.id);
-      updateSpine(this.model.get('decade') + '-' + this.model.get('year'));
+
+    if(window.App.optionClickDev) {
+      if (this.yearToggle === 1) {
+        $(this.el).find('.dive').first().toggleClass('dive__open');
+      } else {
+        $(this.el).find('.dive').first().toggleClass('dive__open');
+        showYear(this.model.id);
+        updateSpine(this.model.get('decade') + '-' + this.model.get('year'));
+      }
+      this.yearToggle ^= 1;
     }
-    this.yearToggle ^= 1;
   },
-  
+
+  launchTap: function(e) {
+    e.preventDefault();
+
+    if(window.App.optionTap) {
+      if (this.yearToggle === 1) {
+        $(this.el).find('.dive').first().toggleClass('dive__open');
+      } else {
+        $(this.el).find('.dive').first().toggleClass('dive__open');
+        showYear(this.model.id);
+        updateSpine(this.model.get('decade') + '-' + this.model.get('year'));
+      }
+      this.yearToggle ^= 1;
+    }
+  },
+
   clapseItemPinch : function(e) {
     e.preventDefault();
     e.stopPropagation();
 
-    $(this.el).find('.dive').first().removeClass('dive__open');
+    if(window.App.optionPinch) {
+      $(this.el).find('.dive').first().removeClass('dive__open');
+    }
   },
 
   preventDefault: function(e) {
     e.preventDefault();
     e.stopPropagation();
-  },  
-  
+  },
+
   render : function() {
     var template =  _.template($("#template-year").html());
     var html = template(this.model.toJSON());
     $(this.el).append(html);
-    
+
     return this;
   }
 });
@@ -558,11 +607,11 @@ App.YearView = Backbone.View.extend({
 App.Router = Backbone.Router.extend({
   routes: {
     'year/:id': 'showYear',
-    '*path': 'defaultRoute'    
+    '*path': 'defaultRoute'
   },
-  
+
   // Show all the chapters (sections) for a year
-  showYear: showYear,  
+  showYear: showYear,
   defaultRoute: function(path) {
 
     var callback = function(decades) {
@@ -570,17 +619,17 @@ App.Router = Backbone.Router.extend({
       $("#decadeIntro").show();
       // $("#chapters").hide();
 
-      var introData = _.reject(_.map(decades, 
-        function(x){ if (_.any(x.tags, function(y){ return y.title == 'intro' })) return x;}), 
+      var introData = _.reject(_.map(decades,
+        function(x){ if (_.any(x.tags, function(y){ return y.title == 'intro' })) return x;}),
         function(z){ return z == undefined }
       )[0];
 
       var decadeSet = _.reject(decades,function(x){ return x.id == introData.id; });
-      
+
       var introDecade = new App.Decade(introData);
       var introView = new App.DecadeIntroView({model: introDecade })
       introView.render();
-      
+
       App.decades = new App.Decades(decadeSet);
       App.decadesView = new App.DecadesView({ collection: App.decades });
 
@@ -590,13 +639,13 @@ App.Router = Backbone.Router.extend({
         var value = value +  100 / len;
         gSpineData[App.decades.models[i].get('decade')] = value;
       }
-      
+
       var allChapters = new App.Chapters(chapters);
 
-      // Update the Spine data for the chapters 
+      // Update the Spine data for the chapters
       // Filter our the parts.
       // This is pretty quick and could be improved.
-      
+
       var x = {}
       for (var i = 0; i < allChapters.models.length; i++) {
         if (allChapters.models[i].get('decade') !== undefined) {
@@ -604,7 +653,7 @@ App.Router = Backbone.Router.extend({
           if (x[allChapters.models[i].get('decade')] === undefined) {
             x[allChapters.models[i].get('decade')] = 0;
           }
-          
+
           if (allChapters.models[i].get('part') == 1) {
             x[allChapters.models[i].get('decade')]++;
             var y = allChapters.models[i].get('year');
@@ -612,23 +661,23 @@ App.Router = Backbone.Router.extend({
             var baseValue = gSpineData[d];
             gSpineData[d+'-'+ y] = y + baseValue;
           }
-        }        
+        }
       }
 
-      $('#decades').html(App.decadesView.render().el);      
+      $('#decades').html(App.decadesView.render().el);
     }
-    
+
 
     if (window.App.online) {
-      $.getJSON(apiEndpoint + '/get_tag_posts/?tag=decade', function(decadeData, status, xhr){ 
-        $.getJSON(apiEndpoint + '?json=1&count=1000', function(chapterData, status, xhr){ 
+      $.getJSON(apiEndpoint + '/get_tag_posts/?tag=decade', function(decadeData, status, xhr){
+        $.getJSON(apiEndpoint + '?json=1&count=1000', function(chapterData, status, xhr){
           // Polute this one..
           // allChapters = chapterData.posts;
           chapters = chapterData.posts;
           callback(decadeData.posts);
         });
       });
-    } else {      
+    } else {
       // Load Book with cached data decade data.
       callback(decadeData);
     }
@@ -637,17 +686,17 @@ App.Router = Backbone.Router.extend({
 });
 
 
-$(function() {  
+$(function() {
   Backbone.emulateJSON = true;
-  
+
   // Initialize the Backbone router.
-  App.router = new App.Router();  
+  App.router = new App.Router();
   Backbone.history.start();
 
 });
 
 var showYear = function(id) {
-    
+
   var chap = new App.Chapters(chapters);
 
   var filteredSet = chap.filter(function(o) {
